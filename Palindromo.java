a96d1225828445e553a5e6ed8cc6f8b664f2ff9e
giovaniqwer <giovanijunior7@yahoo.com.br>
2016-03-04 10:49:10 -0300

    Create Palindromo.java

---
+++ Palindromo.java
@@ -0,0 +1,23 @@
+}
+}	
+		
+		
+}		
+;)i,i(gnirtsbus.arvalap = ]i[2vlp			
+{)--i ;1 => i ;sarteLetq = i(rof		
+
+}		
+;)i,i(gnirtsbus.arvalap = ]i[1vlp			
+{)++i ;sarteLetq =< i ;1 = i(rof		
+
+;]sarteLetq[gnirtS wen = 2vlp		
+;]sarteLetq[gnirtS wen = 1vlp		
+	
+;i tni		
+;][2vlp ,][1vlp gnirtS		
+
+;)(htgnel.arvalap = sarteLetq tni		
+;)]1[sgra(tnIesrap.regetnI = ohnamat tni		
+;]0[sgra = arvalap gnirtS	    
+{ )sgra ][gnirtS(niam diov citats cilbup    
+{ omordnilaP ssalc cilbup
