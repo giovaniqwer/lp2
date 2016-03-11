5705a9a6b708ce2dee8b655f1ffeb6702db35880
giovaniqwer <giovanijunior7@yahoo.com.br>
2016-03-11 11:57:49 -0300

    Create Palindromo.java

---
+++ exercicio4/Palindromo.java
@@ -0,0 +1,30 @@
+public class Palindromo {
+	public int ehpalindromo(String palavra){
+		int qteLetras = palavra.length();
+		String palavra2 = "";
+		String palavra3 = "";
+
+		for(int i=0; i <= qteLetras - 5; i++){
+			palavra2 = palavra.substring(i,i+5);
+			for(int j = 4; j>=0; j--){
+				palavra3 = palavra3 + palavra2.charAt(j);
+			} 
+			if(palavra2.equals(palavra3)){
+				return i;
+			}
+			palavra3 = "";
+		}
+		return -1;
+	}
+
+    public static void main(String[] args) {
+    	String palavra = args[0];
+		Palindromo teste = new Palindromo();
+		int a = teste.ehpalindromo(palavra);
+		if(a == -1){
+			System.out.println("Não é palíndromo!!!");		
+		}else{
+			System.out.println("O palíndromo é : " +palavra.substring(a,a+5));		
+		}
+	}
+}
